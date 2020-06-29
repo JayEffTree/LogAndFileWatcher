aad6ee0bc80422960c131a4a45ef7e2181eee7be
JayEffTree <jfinelli3@gmail.com>
2020-06-29 20:31:32 +0200

    wow a logo

--- resource/embeds.js
+++ resource/embeds.js
@@ -4,6 +4,7 @@ module.exports = {
       .setName("ShareX")
       .setColor("#00ff1a")
       .setTime()
+      .setAvatar('https://getsharex.com/img/ShareX_Logo.png')
       .setImage(`${access_method}://${path.slice(9)}`)
       .setDescription(`File Added: ${access_method}://${path.slice(9)}`);
     return a;
@@ -13,6 +14,7 @@ module.exports = {
       .setName("ShareX")
       .setColor("#ff0000")
       .setTime()
+      .setAvatar('https://getsharex.com/img/ShareX_Logo.png')
       .setDescription(`File Deleted: ${access_method}://${path.slice(9)}`);
     return d;
   }
