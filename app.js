021cca3975b176e480fe3f6d42c8d9d0c562c047
JayEffTree <jfinelli3@gmail.com>
2020-06-29 20:31:21 +0200

    es6 conversion

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ const { addEmbed, delEmbed } = require('./resource/embeds.js');
 const watcher = chokidar.watch(file_upload_dir, { persistent: true, awaitWriteFinish: true });
 const sxHook = new webhook.Webhook(sharex_webhook);
 
-watcher.on('ready', function(){
-  watcher.on('add', function(path) {
+watcher.on('ready', () => {
+  watcher.on('add', (path) => {
     sxHook.send(addEmbed(path, access_method, webhook))
     console.log(`File added: ${path.slice(9)} \nURL: ${access_method}://${path.slice(9)}`)
     return;
   })
-  .on('unlink', function(path) {
+  .on('unlink', (path) => {
     sxHook.send(delEmbed(path, access_method, webhook))
     console.log(`File deleted: ${path.slice(9)} \nURL: ${access_method}://${path.slice(9)}`)
     return;
